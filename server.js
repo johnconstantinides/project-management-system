8661703114885e255ebfcb13d539dfd18db967c7
johnconstantinides <johnconstantinides02@example.com>
2022-01-10 02:17:20 -0500

    hopefully its ready now

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 let Project = require('./models/project.model')
 
 require('dotenv').config();
